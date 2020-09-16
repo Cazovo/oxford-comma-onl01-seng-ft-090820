12832e15e8cb38bf2276d1586bf7ee67fe3555d3
conner fortner <fortner.conner@yahoo.com>
2020-09-16 20:07:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     if array.length == 1
       foods << array[0]
     elsif array.length == 2
-          foods = array.join
+          foods = array.join("and")
     else
       array.each_with_index do |word, index|
         if index == (array.length - 1)
