6b0a4f38885380ffa99923f80693df6086a1ee01
conner fortner <fortner.conner@yahoo.com>
2020-09-16 19:39:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,16 @@
 def oxford_comma(array)
     foods = ""
-    
     lab_array=[]
     if array.length == 1
       puts array[0]
     else
       array.each_with_index do |word, index|
-      if index == (array.length - 1)
-      return "and " + word  
-    else
-      puts 
-    
-    sentence = array.join(", ")
-    puts sentence
+        if index == (array.length - 1)
+         return "and " + word  
+        else
+          puts word + ","
+      end
+    end
 end
 
 oxford_comma(["a", "b", "c"])
