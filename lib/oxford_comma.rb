60ab2cd6229d5a4d54ba3b386798002ee629c401
conner fortner <fortner.conner@yahoo.com>
2020-09-16 19:32:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    foods = ""
+    
+    lab_array=[]
+    if array.length == 1
+      puts array[0]
+    else
+      array.each_with_index do |word, index|
+      if index == (array.length - 1)
+      return "and " + word  
+    else
+      puts 
+    
+    sentence = array.join(", ")
+    puts sentence
 end
+
+oxford_comma(["a", "b", "c"])
